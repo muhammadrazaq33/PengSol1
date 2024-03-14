aa25e3616010afd3754f8a280bd037cae14fe96d
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-03-14 05:01:30 +0500

    All Errors Are resolved

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,16 +4,16 @@ import React from "react";
 const Navbar = () => {
   return (
     <div className="w-[1370px] max-w-[98vw] m-auto h-[10rem] py-4">
-      <div className="flex sm:justify-between justify-center  items-center">
+      <div className="rs4:flex rs4:justify-between justify-center  items-center">
         {/* ImAgeS */}
-        <div className="flex items-center gap-4">
+        <div className="flex items-center justify-center gap-4">
           <img src="./assets/instagram.svg" alt="" />
           <img src="./assets/instagram.svg" alt="" />
           <img src="./assets/instagram.svg" alt="" />
         </div>
 
         {/* lOgO */}
-        <div className="sm:block hidden">
+        <div className="rs4:flex hidden">
           <p className="text-[24px] font-bold header__link2">BUY $PENG</p>
         </div>
       </div>
